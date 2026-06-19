5be9cae091108b2854c15fd2a0586cbacabda7a5
Jef Saber <jef.saber@hotmail.com>
2026-06-19 20:08:29 +0200

    ci: integrate Terraform with Jenkins pipeline

---
+++ infra/variables.tf
@@ -0,0 +1,29 @@
+variable "image_tag" {
+  description = "Tag de l'image Docker à déployer"
+  type        = string
+  default     = "latest"
+}
+
+variable "app_port" {
+  description = "Port exposé en staging"
+  type        = number
+  default     = 8001
+}
+
+variable "container_name" {
+  description = "Nom du conteneur staging"
+  type        = string
+  default     = "sentiment-staging"
+}
+
+variable "registry" {
+  description = "Registry Docker"
+  type        = string
+  default     = "ghcr.io/jefsaber"
+}
+
+variable "docker_host" {
+  description = "Docker host used by Terraform"
+  type        = string
+  default     = "npipe:////./pipe/docker_engine"
+}
